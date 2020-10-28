2820c4dddc00730c014de842e3aac385e0efdc72
Richard <richfir@gmail.com>
2020-10-28 17:29:08 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("share-icon").addEventListener("click", displayShare);
+
+function displayShare(){
+    var x = document.getElementById("share");
+    x.classList.toggle("hidden");
+}
